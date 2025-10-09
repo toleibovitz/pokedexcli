45b714039e4923e25ef79006f97cd6a92d8da21f
toleibovitz <toleibovitz@gmail.com>
2025-10-08 23:46:49 -0500

    removed notes

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -28,7 +28,7 @@ func NewCache(interval time.Duration) *Cache {
 	return &cache
 
 }
-// Create a cache.Add() method that adds a new entry to the cache. It should take a key (a string) and a val (a []byte).
+
 func (c *Cache) Add(key string, val []byte) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -38,7 +38,7 @@ func (c *Cache) Add(key string, val []byte) {
 	}
 	c.cacheMap[key] = entry
 }
-// Create a cache.Get() method that gets an entry from the cache. It should take a key (a string) and return a []byte and a bool. The bool should be true if the entry was found and false if it wasn't.
+
 func (c *Cache) Get(key string) ([]byte, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -50,7 +50,7 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 }
 
 
-// Create a cache.reapLoop() method that is called when the cache is created (by the NewCache function). Each time an interval (the time.Duration passed to NewCache) passes it should remove any entries that are older than the interval. This makes sure that the cache doesn't grow too large over time. For example, if the interval is 5 seconds, and an entry was added 7 seconds ago, that entry should be removed.
+
 func (c *Cache) reapLoop() {
 	ticker := time.NewTicker(c.interval)
 	for range ticker.C {
